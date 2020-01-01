c73fde49f6ac25436ee7615adc27310b9aedbbe8
antonjinji <antonjinji99@gmail.com>
2020-01-01 19:31:05 +0700

    mirror change

--- app/Http/Controllers/QuestionAndEtcController.php
+++ app/Http/Controllers/QuestionAndEtcController.php
@@ -19,6 +19,8 @@ class QuestionAndEtcController extends Controller
      */
     public function index()
     {
+        $questions = Question::paginate(10);
+        return view('homePage')
 
     }
 
